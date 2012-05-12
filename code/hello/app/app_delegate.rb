bf9154035f5a3491abd64e12a7ba9163237a8712
Senthil Nayagam <senthil@railsfactory.org>
2012-05-12 10:09:52 +0530

    Hello World code for the chapter

--- code/hello/app/app_delegate.rb
+++ code/hello/app/app_delegate.rb
@@ -1,5 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    hello = UIAlertView.new
+    hello.message = "Hello World"
+    # hello.dismiss
+    hello.subtitle =  "Subtitle"
+    hello.title =  "Title"
+    hello.show
     true
   end
 end
